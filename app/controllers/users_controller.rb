a718133a4c490358b070e4a014a5d42ae1c285ac
huangxx <huangxiaoxin@cmbc.com.cn>
2015-08-30 22:14:06 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,17 @@ class UsersController < ApplicationController
 	  render 'new'
 	end
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
