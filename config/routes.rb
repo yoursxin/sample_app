841322c7f46d108a538055498fb49897db1c310e
huangxx <huangxiaoxin@cmbc.com.cn>
2015-09-05 21:45:07 +0800

    Add user following -1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only:[:new, :create, :destroy]
   resources :microposts, only:[:create, :destroy]
 
